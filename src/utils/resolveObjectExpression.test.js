fd12ff73ffeb9b228c7b29182104ecc0fab384e3
Alexey Kureev <a.g.kureev@gmail.com>
2017-02-06 16:40:39 +0100

    Reduce the size obj expr spec is taking

--- src/utils/resolveObjectExpression.test.js
+++ src/utils/resolveObjectExpression.test.js
@@ -4,18 +4,9 @@ const babylonConfig = require('../parser/babylon.conf');
 
 test('resolveObjectExpression() resolves nested spreads', () => {
   const code = `
-    const a = {
-      a: 1,
-      b: 2,
-    };
-    const b = {
-      ...a,
-      c: 3
-    };
-    const c = {
-      ...b,
-      d: 4
-    };
+    const a = {a: 1, b: 2};
+    const b = {...a, c: 3};
+    const c = {...b, d: 4};
   `;
   const ast = babylon.parse(code, babylonConfig);
   const objectExpression = ast.program.body[2].declarations[0].init;
